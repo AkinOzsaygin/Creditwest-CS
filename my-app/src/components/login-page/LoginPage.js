efb889587046b5d6ff0dd1e89c0d7322d078a47f
unknown <fethisert0@gmail.com>
2024-08-03 17:00:16 +0300

    batman

--- my-app/src/components/login-page/LoginPage.js
+++ my-app/src/components/login-page/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import '../../css/login-page.css';
-import Logoimage from '../../images/cwlogo.png';
+import logoImage from '../../images/cwlogo.png';
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks.js/useAuth"
 
@@ -50,15 +50,16 @@ const LoginPage = () => {
     };
 
     return (
-        <main className="login-page">
+        <div className="login-page">
             <div className="login-container">
                 <div className="login-image-container">
-                    <img className="login-image" src={Logoimage} alt="CreditWest Software Genius Check Scanner"></img>
-                    <p className="logo-text">CREDİT<span>W</span>EST SOFTWARE GENIUS</p>
+                    <img className="login-image" src={logoImage} alt="CreditWest Software Genius Check Scanner"></img>
                     <p className="check-scanner-text">CHECK SCANNER</p>
+                    <p className="logo-text">CREDİT<span>W</span>EST SOFTWARE GENIUS</p>
+                    
                 </div>
                 <form className="login-form" onSubmit={handleSubmit}>
-                    <h3 className="login-title">Giriş</h3>
+                    <h3 className="login-title">Gİrİş</h3>
                     <p>Lütfen giriş yapmak için kullanıcı adınızı ve şifrenizi giriniz. Eğer kullanıcı adınızı veya şifrenizi bilmiyorsanız lütfen admin ile iletişime geçiniz.</p>
                     <div className="user-info">
                         <label htmlFor="username">Kullanıcı adı</label>
@@ -71,7 +72,7 @@ const LoginPage = () => {
                     <button type="submit" className="login-button">OTURUM AÇ</button>
                 </form>
             </div>
-        </main>
+        </div>
     );
 };
 
